2155e10eba378368a387de0b1b345c334593b16d
Luca Martinelli <martinelliluca98@gmail.com>
2022-08-14 19:08:28 +0200

    Added italy vocabulary

--- ontopia_py/ns.py
+++ ontopia_py/ns.py
@@ -44,6 +44,8 @@ PROVINCES = Namespace(
     "https://w3id.org/italia/controlled-vocabulary/territorial-classifications/provinces/")
 COUNTRIES = Namespace(
     "https://publications.europa.eu/resource/authority/country/")
+ITALY = Namespace(
+    "https://w3id.org/italia/controlled-vocabulary/territorial-classifications/countries/italy/")
 CITIES = Namespace(
     "https://w3id.org/italia/controlled-vocabulary/territorial-classifications/cities/")
 POI_CLASSIFICATION = Namespace(
